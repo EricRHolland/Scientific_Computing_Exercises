ec36ccf69c286bfbe8f000e4d16722382eb169e9
EricRHolland <86615280+EricRHolland@users.noreply.github.com>
2021-12-28 16:11:08 -0600

    skeleton for problem solving

--- Exercise 3/Exercise3_walkthrough.py
+++ Exercise 3/Exercise3_walkthrough.py
@@ -1,1 +1,50 @@
 
+
+class rectangle
+
+
+rect_attributes = ['width','height']
+
+rect_methods = [
+'set_width',
+'set_height',
+'get_area',
+'get_perimeter',
+'get_diagonal',
+'get_picture',
+'get_amount_inside'
+]
+
+area = width * height
+
+perimeter = (2 * width) + (2 * height)
+
+diagonal = ((width ** 2) + (height ** 2)) ** .5
+
+rect_methods = [
+'set_width',
+'set_height',
+'get_area',
+'get_perimeter',
+'get_diagonal',
+'get_picture',
+'get_amount_inside',
+'set_side'
+]
+
+line_creator = ""
+
+end_line = "\n"
+
+create a for loop?
+
+for a in range(0,height):
+    line_creator = "*" * width
+    print(line_creator)
+    if a = height:
+        output.append(line_creator)
+    else:
+        output.append(line_creator, end_line)
+
+
+def shape_calculator():
